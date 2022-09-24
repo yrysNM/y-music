c0146f2bb8d05de19aa822a63f1e0dd89ae82994
yrysNM <yrys.n3m@gmail.com>
2022-09-24 12:31:38 +0600

    block info added

--- client/src/components/app-aboutTwo/AppAboutTwo.js
+++ client/src/components/app-aboutTwo/AppAboutTwo.js
@@ -1,4 +1,8 @@
 import AppAbout from "../app-about/AppAbout";
+import yellowStar from "../../resources/icons/yellowStar.svg";
+import emptyStar from "../../resources/icons/emptyStar.svg";
+import "./appAboutTwo.scss";
+
 const AppAboutTwo = () => {
     const dataAboutTwo = {
         imgUrl: "https://images.unsplash.com/photo-1470225620780-dba8ba36b745?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bXVzaWN8ZW58MHx8MHx8&w=1000&q=80",
@@ -8,9 +12,32 @@ const AppAboutTwo = () => {
     }
 
     return (
-        <>
+        <div className="appAboutOne">
             <AppAbout dataAbout={dataAboutTwo} />
-        </>
+
+
+            <div className="blockSearch blockSearchOrder">
+                <div className="blockSearchOrder-block">
+
+                    <div className="iconStars">
+                        <img src={yellowStar} alt="icon star " />
+                        <img src={yellowStar} alt="icon star " />
+                        <img src={yellowStar} alt="icon star " />
+                        <img src={yellowStar} alt="icon star " />
+                        <img src={emptyStar} alt="icon star " />
+                    </div>
+
+                    <div className="blockSearchOrder-descr">
+                        At vero eos censes aut dolores eos, qui studiose antiqua persequeris, claris et ultimum bonorum.
+                    </div>
+
+                    <div className="blockSearchOrder-personName">
+                        Yrysbek Nessipkulov
+                    </div>
+
+                </div>
+            </div>
+        </div>
     );
 }
 
