317676fc9a6d5408c9ab2fcc5bb6a54915e88963
yrysNM <yrys.n3m@gmail.com>
2022-10-17 08:40:07 +0600

    error with request ymapi

--- client/src/components/page-music/PageMusic.js
+++ client/src/components/page-music/PageMusic.js
@@ -1,7 +1,29 @@
+import { YMApi } from "ym-api";
+import { useEffect } from "react";
 import AudioPlayer from "./components/audio-player/AudioPlayer";
 import AudioLists from "./components/audio-lists/AudioLists";
 import tracks from "./components/data/tracks";
+
+
 const PageMusic = () => {
+    const api = new YMApi();
+
+    useEffect(() => {
+        (async () => {
+            try {
+                await api.init({
+                    username: "6328d8137c954a6aaa3863260eaeefe9",
+                    password: "8d8b050b9320450d9da155753f58cedc"
+                });
+                const result = await api.searchArtists("gorillaz");
+                console.log({ result });
+            } catch (e) {
+                console.log(`api error ${e.message}`);
+            }
+        })();
+
+    })
+
     return (
         <div>
             <AudioPlayer tracks={tracks} />
