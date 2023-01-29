0f4391a61b3ab5bf6b4e74543bad02235406c0a1
yrysNM <yrys.n3m@gmail.com>
2023-01-29 23:58:22 +0600

    song remove comments

--- client/src/reducers/songs.js
+++ client/src/reducers/songs.js
@@ -26,25 +26,4 @@ const songs = createReducer(initialState, {
 )
 
 
-// const songs = (state = initialState, action) => {
-//     switch (action.type) {
-//         case "SONGS_FETCHING":
-//             return {
-//                 ...state,
-//                 songsLoadingStatus: "loading"
-//             }
-//         case "SONGS_FETCHED":
-//             return {
-//                 ...state,
-//                 songsLoadingStatus: "idle"
-//             }
-//         case "SONGS_FETCHING_ERROR":
-//             return {
-//                 ...state,
-//                 songsLoadingStatus: "error"
-//             }
-//         default: return state;
-//     }
-// }
-
 export default songs;
