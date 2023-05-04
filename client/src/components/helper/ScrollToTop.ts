67a34dccbf4d1f1e6c48fbcc1f2c4119124fd27c
yrysNM <yrys.n3m@gmail.com>
2023-05-04 13:03:19 +0600

    scrollBy fixed

--- client/src/components/helper/ScrollToTop.ts
+++ client/src/components/helper/ScrollToTop.ts
@@ -2,13 +2,14 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const ScrollToTop = () => {
-  const location = useLocation();
-
+  const { pathname } = useLocation();
   useEffect(() => {
-    // window.scrollTo(0, 0);
-    window.scrollTo(10, 10);
-  }, [location.pathname]);
-
+    console.log(pathname);
+    /* settimeout make sure this run after components have rendered. This will help fixing bug for some views where scroll to top not working perfectly */
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 0);
+  }, [pathname]);
   return null;
 };
 
