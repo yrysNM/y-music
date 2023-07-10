bbee64f132cad38506f455521adde436be4e3e56
yrysNM <yrys.n3m@gmail.com>
2023-07-10 11:00:03 +0600

    build: change package-lock.json

--- client/y-music-react/src/components/Error.tsx
+++ client/y-music-react/src/components/Error.tsx
@@ -1,7 +1,7 @@
 const Error = () => (
   <div className="w-full flex justify-center items-center">
     <h1 className="font-bold text-2x1 text-white mt-2">
-      Something went wrong. Please try again.
+      Something went wrong. Please try again. Hello from REACT
     </h1>
   </div>
 );
