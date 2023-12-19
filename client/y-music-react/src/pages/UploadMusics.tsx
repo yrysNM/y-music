aed05a34384027f2e9914ab114a3c0f7ad2156af
yrysNM <yrys.n3m@gmail.com>
2023-12-19 19:40:02 +0600

    feat: initial upload music but error type with event

--- client/y-music-react/src/pages/UploadMusics.tsx
+++ client/y-music-react/src/pages/UploadMusics.tsx
@@ -1,41 +1,103 @@
-import { useRef } from "react";
-import { PageTitle } from "../components"
-import { FaUpload } from "react-icons/fa6";
+import {useRef, useState} from 'react';
+import {PageTitle} from '../components';
+import {FaUpload} from 'react-icons/fa6';
 export const UploadMusics = () => {
+  const [isDragOver, setIsDragOver] = useState(false);
 
-    const uploadInput: (React.LegacyRef<HTMLInputElement>) | undefined = useRef(null);
+  const uploadInput: React.LegacyRef<HTMLInputElement> | undefined =
+    useRef(null);
 
-    const upload = (event: React.ChangeEvent<HTMLInputElement>) => {
-        console.log(event);
+  const upload = (
+    event: React.ChangeEvent<HTMLInputElement> | React.DragEvent<HTMLDivElement>
+  ) => {
+    event.stopPropagation();
+    event.preventDefault();
+    console.log(event);
+
+    // const files = 'files' in event.target ? [...event.target.files] : [...event.dataTransfer.files]
+    /**
+     * @TODO change files
+     */
+    let files: File[] | null = null;
+
+    if ('files' in event.target) {
+      /**
+       * @ERROR Type 'FileList | null' must have a '[Symbol.iterator]()' method that returns an iterator.
+       */
+      files = [...event.target.files];
+    } else {
+      event = event as React.DragEvent<HTMLDivElement>;
+      files = [...event.dataTransfer.files];
     }
 
-    const onClickDropbox = (event: React.MouseEvent<HTMLInputElement>) => {
-        console.log(event);
-
-        if (uploadInput.current) {
-
-            console.log(uploadInput.current.click());
-        }
+    if (files) {
     }
+  };
 
+  const onClickDropbox = (event: React.MouseEvent<HTMLInputElement>) => {
+    console.log(event);
 
-    return (
-        <div className="flex flex-col">
-            <PageTitle title="Upload favorite musics" />
+    if (uploadInput.current) {
+      console.log(uploadInput.current.click());
+    }
+  };
 
-            <div className="bg-white/50 rounded border border-gray-200 relative flex flex-col">
-                <div className="px-6 pt-6 pb-5 font-bold border-b border-gray-200 flex flex-row gap-4 items-center">
-                    <span className="card-title text-white">Upload</span>
-                    <FaUpload className="text-white" />
-                </div>
-                <div className="p-6">
-                    {/* upload dropbox */}
-                    <div className="w-full px-10 py-20 rounded text-center cursor-pointer border border-dashed border-gray-400 text-white gray-400 transition duration-500  hover:bg-blue-700 hover:border-blue-700 hover:border-sold" onClick={onClickDropbox}>
-                        <h5>Drop your files here</h5>
-                    </div>
-                    <input type="file" ref={uploadInput} style={{ display: 'none' }} multiple onChange={(e) => upload(e)} />
-                </div>
-            </div>
+  const preventStop = (
+    e: React.DragEvent<HTMLDivElement>,
+    isDragOver?: boolean
+  ) => {
+    e.stopPropagation();
+    e.preventDefault();
+
+    if (typeof isDragOver === 'boolean') {
+      setIsDragOver(isDragOver);
+    }
+  };
+
+  return (
+    <div className="flex flex-col">
+      <PageTitle title="Upload musics" />
+
+      <div className="bg-white/50 rounded border border-gray-200 relative flex flex-col">
+        <div className="px-6 pt-6 pb-5 font-bold border-b border-gray-200 flex flex-row gap-4 items-center">
+          <span className="card-title text-white">Upload</span>
+          <FaUpload className="text-white" />
         </div>
-    )
-} 
+        <div className="p-6">
+          {/* upload dropbox */}
+          <div
+            onClick={onClickDropbox}
+            onDrag={preventStop}
+            onDragStart={preventStop}
+            onDragEnd={(e) => {
+              preventStop(e, false);
+            }}
+            onDragOver={(e) => {
+              preventStop(e, true);
+            }}
+            onDragEnter={(e) => {
+              preventStop(e, true);
+            }}
+            onDragLeave={(e) => {
+              preventStop(e, false);
+            }}
+            onDrop={(e) => upload(e)}
+            className={
+              'w-full px-10 py-20 rounded text-center cursor-pointer border border-dashed border-gray-400 text-white gray-400 transition duration-500  hover:bg-blue-700 hover:border-blue-700 hover:border-sold' +
+              ` ${isDragOver && 'bg-blue-700 border-blue-400 border-sold'}`
+            }
+          >
+            <h5>Drop your files here</h5>
+          </div>
+          <input
+            type="file"
+            ref={uploadInput}
+            style={{display: 'none'}}
+            multiple
+            onChange={(e) => upload(e)}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
